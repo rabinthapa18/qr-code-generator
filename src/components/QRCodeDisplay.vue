d345db93ad36bd48a37bb3ab1fd90f9c04f132bf
Rabinson <rabinson.thapa.18@gmail.com>
2024-09-11 18:35:29 +0900

    feat: function to download qr code

--- src/components/QRCodeDisplay.vue
+++ src/components/QRCodeDisplay.vue
@@ -12,7 +12,16 @@
 
   <!-- dialog dor enlarged qrcode -->
   <div v-if="isDialogOpen" class="dialog-overlay" @click="toggleDialog(false)">
-    <div class="dialog-content">
+    <div
+      class="dialog-content"
+      @mouseenter="toggleDownloadButton(true)"
+      @mouseleave="toggleDownloadButton(false)"
+    >
+      <div class="download-button-wrapper" v-if="isDownloadable">
+        <button class="download-button" @click="downloadQRCode">
+          Download
+        </button>
+      </div>
       <img :src="qrCode" alt="QR Code Enlarged" class="qr-code-enlarged" />
     </div>
   </div>
@@ -22,14 +31,27 @@
 import { ref, defineProps } from "vue";
 
 // define props
-defineProps<{ qrCode: string }>();
+const props = defineProps<{ qrCode: string }>();
 
 // define data
 const isDialogOpen = ref(false);
+const isDownloadable = ref(false);
 
 // define methods
 const toggleDialog = (value: boolean) => {
   isDialogOpen.value = value;
+};
+
+const toggleDownloadButton = (value: boolean) => {
+  isDownloadable.value = value;
+  console.log("hovered");
+};
+
+const downloadQRCode = () => {
+  const link = document.createElement("a");
+  link.href = props.qrCode;
+  link.download = "qrcode.png";
+  link.click();
 };
 </script>
 
@@ -69,10 +91,37 @@ const toggleDialog = (value: boolean) => {
 .dialog-content {
   background-color: #fff;
   border-radius: 4px;
+  position: relative;
 }
 
 .qr-code-enlarged {
   width: 100%;
   height: auto;
+  cursor: pointer;
+}
+
+.download-button-wrapper {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.download-button {
+  background-color: #9251f9;
+  color: #ffffff;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.download-button:hover {
+  background-color: #7b3dbf;
 }
 </style>
